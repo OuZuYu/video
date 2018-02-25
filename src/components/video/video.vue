1d87a7f694f3735c001296c677521785e2ba06ac
ouJoy <oupenguin@gmail.com>
2018-02-25 17:40:04 +0800

    增加上拉加载

--- src/components/video/video.vue
+++ src/components/video/video.vue
@@ -1,40 +1,72 @@
 <template>
 	<div class="video">
-		<ul class="video-list">
-			<li class="video-item" v-for="(item, index) in videos.data" :key="index" @click="playVideo(item)">
-				<div class="img-wrapper">
-					<img :src="item.coverUrl">
-				</div>
-				<div class="title">
-					{{item.name}}
-				</div>
-				<div class="hot" v-if="item.hot">
-					<i class="iconfont">&#xe7d9;</i>
-				</div>
-			</li>
-		</ul>
+		<div class="video-wrapper">
+			<div>
+				<ul class="video-list">
+					<li class="video-item" v-for="(item, index) in videos.data" :key="index" @click="playVideo(item)">
+						<div class="img-wrapper">
+							<img :src="item.coverUrl">
+						</div>
+						<div class="title">
+							{{item.name}}
+						</div>
+						<div class="hot" v-if="item.hot">
+							<i class="iconfont">&#xe7d9;</i>
+						</div>
+					</li>
+				</ul>
+				<div class="bottom-tip">
+			        <span>{{loadTip}}</span>
+			    </div>
+			</div>
+		</div>
 		<player ref="player" :videoData="selectedVideo"></player>
 	</div>
 </template>
 
 <script>
 import player from '../video-player/player'
+import BScroll from 'better-scroll'
 	export default {
 		data () {
 			return {
 				videos: [],
-				selectedVideo: {}
+				selectedVideo: {},
+				page: 1,
+				bscroll: null,
+				loadTip: '---上拉加载更多---'
 			}
 		},
 		mounted () {
 			this.getVideos();
+			this.bscroll = new BScroll('.video-wrapper', {
+			    probeType: 1,
+			    click: true
+			});
+			this.bscroll.on('touchEnd', (position) => {
+				if(position.y < (this.bscroll.maxScrollY - 30)) {
+			    	if (this.loadTip === '---没有更多了---') return;
+			    	this.loadTip = '加载中...';
+			        this.page++;
+			        this.getVideos();
+			        this.loadTip = '---上拉加载更多---';
+			        this.$nextTick(() => {
+			        	this.bscroll.refresh()
+			        });
+			    }
+			});
 		},
 		methods: {
 			getVideos () {
-				this.$http.get('/mock/videos').then(({data}) => {
+				this.$http.post('/mock/videos', {page: this.page}).then(({data}) => {
 					console.log('video', data);
-					this.videos = data;
-					console.log(this.videos)
+					if (this.page===1) {
+						this.videos = data;
+					} else if (this.page === 2) {
+						this.videos.data.push(...data.data);
+					} else {
+						this.loadTip = "---没有更多了---"
+					}
 				})
 			},
 			playVideo (data) {
@@ -49,36 +81,51 @@ import player from '../video-player/player'
 </script>
 
 <style scoped lang="scss">
-.video-list {
-	.video-item {
-		position: relative;
-		display: inline-block;
-		width: 44%;
-		height: 165px;
-		margin: 0 0 10px 4%;
-		border-radius: 6px;
-		background: #fff;
-		.img-wrapper {
-			height: 80%;
-			img {
-				width: 100%;
-				height: 100%;
+.video-wrapper {
+	position: fixed;
+	top: 120px;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	.video-list {
+		.video-item {
+			position: relative;
+			display: inline-block;
+			width: 44%;
+			height: 165px;
+			margin: 0 0 10px 4%;
+			border-radius: 6px;
+			background: #fff;
+			.img-wrapper {
+				height: 80%;
+				img {
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.title {
+				height: 20%;
+				padding: 4px;
+				box-sizing: border-box;
+				font-size: 12px;
+			}
+			.hot {
+				position: absolute;
+				right: -4px;
+				top: -6px;
+				.iconfont {
+					color: #FA7198;
+				}
 			}
 		}
-		.title {
-			height: 20%;
-			padding: 4px;
-			box-sizing: border-box;
-			font-size: 12px;
-		}
-		.hot {
-			position: absolute;
-			right: -4px;
-			top: -6px;
-			.iconfont {
-				color: #FA7198;
-			}
-		}
+	}
+	.bottom-tip {
+		width: 100%;
+	    height: 35px;
+	    line-height: 35px;
+	    text-align: center;
+	    color: #777;
+	    background: #f2f2f2;
 	}
 }
 </style>
--- src/mock.js
+++ src/mock.js
@@ -1,11 +1,13 @@
 import Mock from 'mockjs';
 
-function video () {
-	let videosData = {
+function video (op) {
+	console.log(op.body);
+	let page1 = {
 		data: [
 			{
 				name: '测试视频',
-				videoUrl: 'http://p412fe7la.bkt.clouddn.com/smile.mov',
+				//videoUrl: 'http://p412fe7la.bkt.clouddn.com/smile.mov',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
 				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
 				hot: 1,
 				desc: '这是视频描述哈哈哈哈哈'
@@ -54,9 +56,70 @@ function video () {
 			}
 		]
 	}
-	return videosData;
+	let page2 = {
+		data: [
+			{
+				name: '第二页内容',
+				//videoUrl: 'http://p412fe7la.bkt.clouddn.com/smile.mov',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				hot: 1,
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			},
+			{
+				name: '第二页内容',
+				videoUrl: 'http://p412fe7la.bkt.clouddn.com/iflove.mp4',
+				coverUrl: 'http://p412fe7la.bkt.clouddn.com/20170505094502_tRmHC.thumb.700_0.jpeg',
+				desc: '这是视频描述哈哈哈哈哈'
+			}
+		]
+	}
+	let page = JSON.parse(op.body).page;
+	if (page === 1) {
+		return page1;
+	} else if (page === 2) {
+		return page2;
+	} else {
+		return false;
+	}
 }
-Mock.mock('/mock/videos', 'get', video);
+Mock.mock('/mock/videos', video);
 
 function img () {
 	let imgsData = {
